b96042724a3b096d1fd103b3218927a3b67eed10
Mike Faust <mike@elfausto.net>
2017-01-14 00:41:56 -0500

    Intial load of trailhead project

---
+++ src/aura/InTheArea/InTheAreaHelper.js
@@ -0,0 +1,60 @@
+({
+    getLocalList: function(component) {
+        var spinner = component.find('spinner');
+        $A.util.removeClass(spinner,"slds-hide");
+        
+        var objectType = component.get("v.sObjectName");
+        //var action = component.get("c.getLocal");
+        var searchTerm = component.find("searchTerm").get("v.value");
+        if (!searchTerm) {
+            searchTerm = component.get("v.defaultSearch");
+        }
+        if(recID) {
+            var action = component.get("c.getListByAddress");
+            action.setParams({"recordId": recID, "objectType": objectType, "searchQuery":searchTerm});
+        }
+        else 
+        {
+            var location = component.get("v.location");
+            var action = component.get("c.getLocal");
+        action.setParams({
+            "searchTerm" : searchTerm,
+            "lat": location.coords.latitude,
+            "long": location.coords.longitude
+            
+        });
+        }
+       // location = JSON.parse(location);
+       /* var action = component.get("c.getListByAddress");
+        action.setParams({
+            "recordId" : recID,
+            "objectType": objectType,
+            "searchQuery": searchTerm
+            
+        });*/
+        /*action.setParams({
+            "searchTerm": searchTerm,
+            "lat": location.coords.latitude,
+            "lon": location.coords.longitude
+        });*/
+        action.setCallback(this, function(response) {
+            this.doLayout(response, component);
+        });
+        $A.enqueueAction(action);
+    },
+    doLayout: function(response, component) {
+        var data = JSON.parse(response.getReturnValue());
+        var warning = component.find('warning');
+        if (data.error) {
+            component.set("v.errorMessage", data.error);
+            $A.util.removeClass(warning, 'slds-hide');
+        }
+        else {
+            $A.util.addClass(warning, 'slds-hide');
+        }
+        component.set("v.restaurantList", data.bizArray);
+        console.log("The Data: ", data);
+        var spinner = component.find('spinner');
+        $A.util.addClass(spinner,"slds-hide");
+    }
+})
